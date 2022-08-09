8164e1c207a0d2deda549931a9fd6f6a8235d593
AlexanderYanakov <yanakov_222@mail.ru>
2022-08-09 18:33:20 +0300

    fix

--- ImageResizerService/Service/ResizeService.cs
+++ ImageResizerService/Service/ResizeService.cs
@@ -9,11 +9,8 @@ namespace ImageResizerService.Service
         public void ConvertImage(IFormFile image)
         {
             StreamWriter sw = new StreamWriter("C:/Users/Alexander/Desktop/ConvertedImages/Images.txt");
-            //Write a line of text
-            sw.Write("Photo is downloaded");
-            //Write a second line of text
+            sw.WriteLine("Photo is downloaded");
             sw.WriteLine("From the StreamWriter");
-            //Close the file
             sw.Close();
         }
     }
